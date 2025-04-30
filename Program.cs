1041a4ffc3ea0c859831c8c0dcd459b61201aba2
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-04-30 15:43:21 +0700

    initialize code

--- Program.cs
+++ Program.cs
@@ -7,8 +7,7 @@ namespace MOD10_103022300125
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
+            // Add services to the container
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
